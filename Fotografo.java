46d68a63307d5f88e3c08dc28b6ca135e1ae90ae
alexnalfaro <93652575+alexnalfaro@users.noreply.github.com>
2022-05-24 18:10:56 +0200

    Add files via upload

--- Fotografo.java
+++ Fotografo.java
@@ -9,28 +9,27 @@ import java.util.ArrayList;
 
 public class Fotografo 
 {
-	Connection con;
-	Statement st;
-	ResultSet rs;
-	public void AbrirConexion()
-	{
+	public static ArrayList<String> listaFotografo() 
+	{	
+		ArrayList<String> a = new ArrayList<String>();
 		String url = "jdbc:mysql://localhost/JDBC_ALEX_EJERCICIO1";
 		String user = "root";
 		String psswd = "root";
+		Connection con;
 		try {
 			con = DriverManager.getConnection(url,user,psswd);
 			System.out.println("Conectado");
-			st = con.createStatement();
+			Statement st = con.createStatement();
+			String query = "SELECT nombre from fotografos";
+			ResultSet rs = st.executeQuery(query);
+			while(rs.next())
+			{
+				String nombre = rs.getString("nombre");
+				a.add(nombre);
+			}
 		} catch (SQLException e) {
-			// TODO Bloque catch generado automáticamente
-			e.printStackTrace();
+			System.err.print("No se ha podido conectar con la base de datos");
 		}
-	}
-	public static ArrayList<String> listaFotografo() 
-	{
-		
-		
-		ArrayList<String> a = new ArrayList<String>();
 		return a;
 	}
 }
--- VerImagen.java
+++ VerImagen.java
@@ -2,6 +2,7 @@ package EJERCICIO2;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -12,11 +13,12 @@ public class VerImagen extends JFrame
 	JPanel panel3 = new JPanel();
 	JPanel panel4 = new JPanel();
 	ArrayList<String> fotografos = new ArrayList<String>();
-	public VerImagen()
+	public VerImagen() throws SQLException
 	{
 		setSize(500,400);
 		setTitle ("Ejercicio2");
 		setLayout(new GridLayout(2,2));
+		//PANEL 1 (ARRIBA IZQUIERDA)
 		panel1.setLayout(new GridLayout(1,2));
 		JLabel labelfoto = new JLabel ("Fotografo: ");
 		panel1.add(labelfoto);
@@ -26,10 +28,24 @@ public class VerImagen extends JFrame
 		{
 			combo.addItem(fotografos.get(i));
 		}
-		
-		
+		panel1.add(combo);
+		add(panel1);
+		//PANEL 2 (ARRIBA DERECHA)
+		panel2.setLayout(new GridLayout(2,2));
+		JLabel labelfecha = new JLabel ("Fotos despues de: ");
+		panel2.add(labelfecha);
+		add(panel2);
+		//PANEL 3 (ABAJO IZQUIERDA)
+		panel3.setLayout(new GridLayout(1,1));
+		JButton a = new JButton ("A");
+		panel3.add(a);
+		add(panel3);
+		//PANEL 4 (ABAJO DERECHA)
+		panel4.setLayout(new GridLayout(1,1));
+		JButton b = new JButton ("B");
+		panel4.add(b);
+		add(panel4);
 	}
-	
 	public static void main(String[] args) 
 	{
 		EventQueue.invokeLater(new Runnable() 
